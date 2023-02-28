246819b28c9dde7e67e149956913ebc20e4a68ad
Brynfor Morris <brynfor.morris@gmail.com>
2023-02-27 18:52:04 -0600

    FilterDeliverable Met

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,12 +5,31 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [filterString, setFilterString] = useState("");
 
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
+  function handleCategoryChange(category) {
+    setSelectedCategory(category);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  function handleUserSearch(searchString){
+    setFilterString(searchString);
+  }
+
+  // PseudoCode
+  // Store each typed letter as a string in state,
+  // declare variable and set equal to filter
+  // use the .filter method and pass it a function that returns
+    // true or false
+  // set state string and comparison category to lower case
+  // use string.includes() also to compare
+  // return true if conditions are met
+  // pass through line 30
+
+  const filteredList = items.filter((item) => {
+    return item.name.toLowerCase().includes(`${filterString}`)
+  })
+
+  const itemsToDisplay = filteredList.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
@@ -19,7 +38,11 @@ function ShoppingList({ items }) {
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter 
+        onCategoryChange={handleCategoryChange}
+        handleUserSearch={handleUserSearch}
+        filteredItem = {filterString}
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
